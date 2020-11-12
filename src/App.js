732ce3f181ab933c61bdfa16ec5f0115fbe92fc0
awalemandi <mandi.awale@gmail.com>
2020-11-12 11:08:22 +1100

    created navbar and navbar components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar/>
-      <h1>Portfolio</h1>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavButton,
+    NavButtonLink
+} from './NavbarElements';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Nav</h1>
-        </div>
-    )
-}
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>Awale</NavLogo>
+                    <MobileIcon>
+                        <FaBars/>
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='skills'>Skills</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='projects'>Projects</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavButton>
+                                <NavButtonLink to='contact'>Contact</NavButtonLink>
+                            </NavButton>
+                        </NavItem>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
 
-export default Navbar
+export default Navbar;

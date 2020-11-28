fba5e7edf3d749b8d5a8665aa7f91e75518a8d06
Joanna Cheok <jo@Joannas-MacBook-Pro.local>
2020-11-28 12:08:19 +1100

    added netlify props to jsx form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,8 +5,13 @@ import { RiSendPlaneFill } from 'react-icons/ri';
 const ContactForm = () => {
     return (
         <>
-            <form name='contactForm' method='post'>
-                <input type="hidden" name="form-name" value="contactForm" />
+            <form
+                name='contact'
+                method='post'
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+            >
+                <input type='hidden' name='form-name' value='contact' />
                 <Input type='text' placeholder='Name' name='name' />
                 <Input type='email' placeholder='Email' name='email' />
                 <TextArea placeholder='Message' name='message' />
